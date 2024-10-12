96e0a23a47b9d6ee78a101c38a962cddf0738890
marsad <marsad11223@gmail.com>
2024-10-12 17:22:48 +0500

    section newdishes

--- src/app/homePage/_sections/NewDishes.tsx
+++ src/app/homePage/_sections/NewDishes.tsx
@@ -3,41 +3,90 @@ import { Box, Typography } from "@mui/material";
 import Image from "next/image";
 
 export default function NewDishes() {
+  const heddingBox = {
+    height: { xs: "55px", sm: "98px", lg: "117px" },
+    width: { xs: "220px", sm: "400px", lg: "490px" },
+    clipPath: "polygon(0 0, 100% 0, 97% 100%, 3% 100%)",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate( -50% , -50% )",
+  };
   return (
     <>
-      <Box sx={{ padding: "0px 80px 80px 80px", backgroundColor: "red" }}>
+      <Box
+        sx={{
+          position: "relative",
+        }}
+      >
+        {/* background image  */}
         <Box
-          sx={{ textAlign: "center", padding: "100px 0", position: "relative" }}
+          sx={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            opacity: "2",
+            objectFit: "cover",
+          }}
+        >
+          <Image
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            src={pngs.BGwelcomeSabri}
+            alt="BGwelcomeSabri"
+          />
+        </Box>
+        <Box
+          sx={{
+            textAlign: "center",
+            padding: { xs: "40px 0", sm: "85px 0", lg: "100px 0" },
+            position: "relative",
+          }}
         >
           <Typography
             sx={{
               color: "#851A1D",
-              fontSize: "64px",
+              fontSize: { xs: "24px", sm: "35px", md: "45px", lg: "64px" },
               fontWeight: "700",
               fontFamily: "Oswald",
+              position: "relative",
+              zIndex: "10",
             }}
           >
             OUR NEW DISHES
           </Typography>
+          {/* hedding box  */}
           <Box
             sx={{
-              height: "117px",
-              width: "490px",
+              ...heddingBox,
+              zIndex: "9",
               backgroundColor: "#ffffff",
-              clipPath: "polygon(0 0, 100% 0, 97% 100%, 3% 100%)",
-              position: "absolute",
-              top: "-50px",
-              left: "-50%",
             }}
-          >
-            <Box
-              sx={{ width: "100%", height: "100%", position: "absolute" }}
-            ></Box>
-          </Box>
+          ></Box>
+          <Box
+            sx={{
+              ...heddingBox,
+              marginTop: { xs: "9px", lg: "12px" },
+              width: { xs: "227px", sm: "420px", lg: "500px" },
+              zIndex: "8",
+              backgroundColor: "#851A1D",
+            }}
+          ></Box>
         </Box>
 
         {/* dishes menu  */}
-        <Box sx={{ height: "auto", width: "100%" }}>
+        <Box
+          sx={{
+            height: "auto",
+            width: "100%",
+            position: "relative",
+            opacity: "5",
+            padding: {
+              xs: "0 20px 20px 20px ",
+              sm: "0 50px 50px 50px ",
+              lg: "0px 80px 80px 80px",
+            },
+          }}
+        >
           <Image
             style={{ width: "100%", height: "100%" }}
             src={pngs.Dishes}
