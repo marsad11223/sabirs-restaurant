75b5cbbc66cc7070a75bafc12a4288ab0e64f16c
marsad <marsad11223@gmail.com>
2024-10-17 16:35:42 +0500

    updata slider

--- src/app/homePage/_sections/FoodItemMarquee.tsx
+++ src/app/homePage/_sections/FoodItemMarquee.tsx
@@ -7,6 +7,9 @@ import { Box, Typography } from "@mui/material";
 import Image, { StaticImageData } from "next/image";
 import Marquee from "react-fast-marquee";
 import { scrollToId } from "@/app/utils/helpers";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 interface FoodCardProps {
   title: string;
@@ -59,20 +62,48 @@ export default function HeroSection() {
     },
   ];
 
-  const [play, setPlay] = useState(true);
-
-  useEffect(() => {
-    const togglePlayState = () => {
-      setPlay(false);
-      setTimeout(() => {
-        setPlay(true);
-      }, 2000);
-    };
-
-    const intervalId = setInterval(togglePlayState, 5000);
-
-    return () => clearInterval(intervalId);
-  }, []);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          autoplaySpeed: 2300,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          autoplaySpeed: 2300,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplaySpeed: 2300,
+        },
+      },
+    ],
+  };
 
   return (
     <>
@@ -80,7 +111,7 @@ export default function HeroSection() {
         sx={{
           background:
             "radial-gradient(50% 95.48% at 50% 50%, #851A1D 0%, #560508 71%)",
-          padding: "20px 0 8px 0",
+          padding: "30px 0 26px 0",
           marginTop: { md: "-4px" },
         }}
       >
@@ -88,6 +119,7 @@ export default function HeroSection() {
           sx={{
             textAlign: "center",
             marginBottom: { xs: "10px", sm: "15px", md: "20px" },
+            width: "100%",
           }}
         >
           <Typography
@@ -101,16 +133,13 @@ export default function HeroSection() {
             OUR CUISINE COLLECTION
           </Typography>
         </Box>
-        <Marquee
-          gradient={false}
-          speed={100}
-          play={play}
-          pauseOnHover={true} // Optional: Pause on hover
-        >
+        <Slider {...settings}>
           {foodItems.map((item, index) => (
-            <FoodCard key={index} title={item.title} image={item.image} />
+            <div key={index}>
+              <FoodCard title={item.title} image={item.image} />
+            </div>
           ))}
-        </Marquee>
+        </Slider>
       </Box>
     </>
   );
@@ -118,51 +147,57 @@ export default function HeroSection() {
 
 export const FoodCard = ({ title, image }: FoodCardProps) => {
   return (
-    <Box
-      onClick={() => scrollToId("location")}
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        // maxWidth: {
-        //   xs: "150px",
-        //   sm: "220px",
-        //   md: "268px",
-        //   lg: "30vw",
-        //   xl: "300px",
-        // },
-        padding: { xs: "0 15px", sm: "0 25px", lg: "0 30px", xl: "0 50px" },
-        cursor: "pointer",
-      }}
-    >
-      {/* Image */}
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
       <Box
+        onClick={() => scrollToId("location")}
         sx={{
-          width: { xs: "145px", sm: "200px", md: "268px" },
-          height: "auto",
-          maxHeight: "268px",
-          paddingBottom: { xs: "8px", sm: "15px", md: "30px" },
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          // width: {
+          //   xs: "150px",
+          //   sm: "220px",
+          //   md: "268px",
+          //   lg: "300px",
+          // lg: "20vw",
+          // xl: "16vw",
+          // },
+          // width: "100%",
+          // padding: { xs: "0 15px", sm: "0 25px", lg: "0 30px", xl: "0 50px" },
+          cursor: "pointer",
         }}
       >
-        <Image
-          style={{ width: "100%", height: "100%" }}
-          src={image}
-          alt={title}
-        />
-      </Box>
+        {/* Image */}
+        <Box
+          sx={{
+            width: { xs: "145px", sm: "200px", md: "268px" },
+            height: "auto",
+            maxHeight: "268px",
+            paddingBottom: { xs: "8px", sm: "15px", md: "30px" },
+          }}
+        >
+          <Image
+            style={{ width: "100%", height: "100%" }}
+            src={image}
+            alt={title}
+          />
+        </Box>
 
-      {/* Title */}
-      <Typography
-        sx={{
-          fontSize: { xs: "16px", sm: "20px", md: "24px" },
-          fontWeight: "600",
-          color: "#FFD40D",
-          backgroundColor: "#000000",
-          padding: "0 7px",
-        }}
-      >
-        {title}
-      </Typography>
+        {/* Title */}
+        <Typography
+          sx={{
+            fontSize: { xs: "16px", sm: "20px", md: "24px" },
+            fontWeight: "600",
+            color: "#FFD40D",
+            backgroundColor: "#000000",
+            padding: "0 7px",
+          }}
+        >
+          {title}
+        </Typography>
+      </Box>
     </Box>
   );
 };
