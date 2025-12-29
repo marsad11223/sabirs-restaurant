cee8e1aa7bbe43547507f19938bb618fadf59634
Marsad Ghanvi <marsad11223@gmail.com>
2025-12-30 02:18:39 +0500

    section CravingSomethingDeliciousNow

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -4,15 +4,17 @@ import HeroLandingPage from "./_sections/HeroLandingPage";
 import SabirsGrillYourChoice from "./_sections/SabirsGrillYourChoice";
 import CustomerFavoritesAlwaysFresh from "./_sections/CustomerFavoritesAlwaysFresh";
 import CelebrateTogetherAtSabirs from "./_sections/CelebrateTogetherAtSabirs";
+import CravingSomethingDeliciousNow from "./_sections/CravingSomethingDeliciousNow";
 
 export default function LandingPage() {
   return (
     <Box>
-      <CelebrateTogetherAtSabirs />
+      <CravingSomethingDeliciousNow />
       <HeroLandingPage />
       <SabirsGrillYourChoice />
       <CustomerFavoritesAlwaysFresh />
       <CelebrateTogetherAtSabirs />
+      <CravingSomethingDeliciousNow />
       <Footer applyBorderTop={false} />
     </Box>
   );
